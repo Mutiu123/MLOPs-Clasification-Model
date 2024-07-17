7716fd9d78d7c39387b38a491d2c08678e208b01
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-07-17 20:23:02 +0100

    modify demo file

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ from us_visa.logger import logging
 from us_visa.exception import USvisaException
 
 
-#logging.info("logging info testing")
+logging.info("logging info testing")
 
 try: 
     b = 5/0
