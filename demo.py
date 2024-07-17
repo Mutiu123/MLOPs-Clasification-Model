b3eab8394ba8229151ce454cfe056fbcaa2e8567
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-07-17 16:18:05 +0100

    create logging and exception modules

---
+++ demo.py
@@ -0,0 +1,13 @@
+
+#from us_visa.logger import *
+import sys
+from us_visa.logger import logging 
+from us_visa.exception import USvisaException
+
+
+#logging.info("logging info testing")
+
+try: 
+    b = 5/0
+except Exception as e:
+    raise USvisaException(e, sys)
